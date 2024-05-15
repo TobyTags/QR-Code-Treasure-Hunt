99f6f06bba0fab7e6cab779edccbcb64cfa928d1
Toby Tangney <TobyTangney@gmail.com>
2024-05-15 20:57:22 +0100

    asd

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
-import { firestore } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
