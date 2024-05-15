4e2ebe1a8ced4100d1e7dc644c32d8013364b70c
Toby Tangney <TobyTangney@gmail.com>
2024-05-15 20:14:02 +0100

    sdf

--- script.js
+++ script.js
@@ -4,7 +4,40 @@ document.addEventListener('DOMContentLoaded', function() {
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent form submission
 
-        alert('Login successful!'); // Display alert message
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // Sign in with email and password
+        auth.signInWithEmailAndPassword(username, password)
+        .then((userCredential) => {
+            // Signed in
+            alert('Login successful!');
+            window.location.href = 'UserPage.html'; // Redirect to dashboard page
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            alert(errorMessage);
+        });
     });
 });
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCC-LVJYx8c0yQZAbJto1D6Pqc4FF0jO08",
+  authDomain: "scavengerhunt-a0e18.firebaseapp.com",
+  projectId: "scavengerhunt-a0e18",
+  storageBucket: "scavengerhunt-a0e18.appspot.com",
+  messagingSenderId: "1057596154789",
+  appId: "1:1057596154789:web:34ef0d1439ea04d48d3e4c",
+  measurementId: "G-PLF992LLML"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
